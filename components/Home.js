1a5d2e381f556992990ff631ab32cbedea62de5b
Richard McGhee <richardjmcghee@yahoo.com>
2021-11-29 12:29:21 -0500

    Getting started with basic components folder, also added our keys component and started the keys array to map over for setting up our keyboard layout

---
+++ components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Keys from './Keys';
+
+function Home() {
+    return ( 
+        <div>
+            <Keys />
+        </div>
+    )
+}
+
+export default Home
---
+++ components/Keys.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Keys() {
+    let keyData = [
+        ろ,　ぬ
+    ]
+    return ( 
+        <div>
+            {/* keyData.map */}
+        </div>
+    );
+}
+
+export default Keys;
