a3e3298e46e2098d6fd63b247adbdae67700a6bd
Richard McGhee <richardjmcghee@yahoo.com>
2021-12-01 00:29:37 -0500

    Added in a link to the data set for future devs or people in need of a JP Keyboard data set.

--- components/Home.js
+++ components/Home.js
@@ -4,6 +4,9 @@ import Keys from './Keys'
 function Home() {
     return ( 
         <div>
+            <h1>
+                <a href="https://github.com/Richard-McGhee/Dark-JP-Keyboard/blob/main/components/Keys.js">Link to Keys Data Set for Reference</a>
+            </h1>
             <Keys />
         </div>
     )
